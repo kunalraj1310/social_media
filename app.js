832a8c195f7b3ef41d89c5e254d668adc6830253
kunalraj1310 <kunalraj13102011@gmail.com>
2026-06-06 21:13:14 +0530

    Set views directory for EJS templating

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, 'views'));
 
 app.set('view engine', 'ejs');
 
